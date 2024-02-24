f2845ca6a792ec44da640c982e6a650bca57f8e7
CrodinMWAS <MWASiskola@gmail.com>
2024-02-24 19:28:57 +0100

    Lot of changes, lost track

---
+++ src/components/Employee.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="employeeCard">
+        <div class="employeePicture" :style="{ backgroundImage: `url(../../src/assets/images/Person${image}vector.svg)` }"></div>
+        <div class="headerContainer">
+        <h1>{{ name }}</h1>
+        <h2>{{ title }}</h2>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["image","name","title"],
+}
+</script>
+
+<style>
+
+</style>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -8,7 +8,7 @@
             <div class="cell"> <h3 class="date">2019</h3> </div>
             <div class="cell"> <h3>Culinary Connections Made</h3> <div class="lineContainer"><div class="line"><div class="circle"></div></div></div> </div>
             <div class="cell"> <h3 class="date">2021</h3> </div>
-            <div class="cell"> <h3>Redifining Dining Landscape</h3> <div class="lineContainer"><div class="line"><div class="circle"></div></div></div> </div>
+            <div class="cell"> <h3>Redefining Dining Landscape</h3> <div class="lineContainer"><div class="line"><div class="circle"></div></div></div> </div>
             <div class="cell"> <h3 class="date">2023</h3> </div>
         </div>
         <div id="bottomRow">
@@ -16,18 +16,101 @@
             <div class="cell"> <h3 class="date">2020</h3> </div>
             <div class="cell"> <div class="lineContainer"><div class="line"><div class="circle"></div></div></div> <h3>Curatorial Excellence Emerges</h3> </div>
             <div class="cell"> <h3 class="date">2022</h3> </div>
-            <div class="cell"> <div class="lineContainer"><div class="line"><div class="circle"></div></div></div> <h3>Redifining Dining Landscape</h3> </div>
+            <div class="cell"> <div class="lineContainer"><div class="line"><div class="circle"></div></div></div> <h3>Redefining Dining Landscape</h3> </div>
         </div>
+    </div>
+
+    <div class="headerContainer">
+        <h1>Meet our team</h1>
+        <h2>The Culinary Visionaries Behind DineEase</h2>
+    </div>
+    <div id="employeeContainer">
+        <Employee v-for="(employee, index) in employees" :key="index" :image="employee.image" :name="employee.name" :title="employee.title" ></Employee>
+    </div>
+
+    <div id="connectionContainer">
+        <h1>Connect with DineEase</h1>
+        <p>Hungry for more? Whether you have questions, feedback, or simply wish to share your culinary story, we're eager to hear from you. Dive deeper into the DineEase experience and let's make every meal memorable.</p>
+        <div id="connectionButton"><h1>Connect With DineEase</h1></div>
     </div>
 </template>
 
 <script>
+import Employee from '../components/Employee.vue';
+
 export default {
-    
+    components:{
+    Employee
+},
+    data() {
+        return {
+            employees: [
+                    {
+                        image: 1,
+                        name: "Elena Martinez",
+                        title: "Founder & CEO"
+                    },
+                    {
+                        image: 2,
+                        name: "Raj Singh",
+                        title: "Chief Culinary Curator"
+                    },
+                    {
+                        image: 3,
+                        name: "Sophia Dawson",
+                        title: "Director of Community Engagement"
+                    },
+                    {
+                        image: 4,
+                        name: "Lucas Kim",
+                        title: "Head of Digital Strategy"
+                    },
+                    {
+                        image: 5,
+                        name: "Aisha Abdi",
+                        title: "Marketing & Brand Manager"
+                    },
+                ],
+        }
+    },
 }
 </script>
 
 <style>
+
+#connectionContainer{
+    width: 100%;
+    height: 40vh;
+    background-color: var(--purpleColor);
+    color: white;
+    padding: 20px 4%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    gap: 0.67em;
+}
+
+#connectionContainer > h1{
+    margin: 0;
+}
+
+#connectionContainer #connectionButton{
+    background-color: var(--greyishWhiteColor);
+    border-radius: 5px;
+    padding: 1% 5%;
+    color: var(--purpleColor);
+}
+
+#connectionContainer #connectionButton h1{
+    font-size: medium;
+}
+
+/*=============================================
+=            Style of the Timeline            =
+=============================================*/
+
     #timeline{
         width: 100%;
         height: 400px;
@@ -70,6 +153,8 @@ export default {
         justify-content: flex-start;
     }
 
+/*----------  Style of the circle ----------*/
+
     #bottomRow .cell::after{
         content: "";
         background-color: var(--purpleColor);
@@ -83,6 +168,21 @@ export default {
         right: calc(50% - 15px);
     }
 
+/*------------------------------------------*/
+
+    .cell .date{
+        font-weight: 900;
+        color: var(--purpleColor);
+    }
+    
+    .cell h3{
+        font-weight: normal;
+        margin: 10px;
+        font-size: small;
+    }
+    
+/*----------  Style of the grey line  ----------*/
+    
     .cell .lineContainer{
         height: 60%;
     }
@@ -104,14 +204,35 @@ export default {
         border-radius: 50%;
     }
 
-    .cell .date{
-        font-weight: 900;
-        color: var(--purpleColor);
+/*=====  End of Style of the Timeline  ======*/
+
+
+/*=============================================
+=            Style of the Employee section            =
+=============================================*/
+
+    .employeeCard{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .employeePicture{
+        width: 75%;
+        aspect-ratio: 1;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        margin-bottom: 1rem;
     }
     
-    .cell h3{
-        font-weight: normal;
-        margin: 10px;
-        font-size: small;
+    .employeeCard .headerContainer h1{
+        margin: 0;
     }
+
+/*=====  End of Style of the Employee section  ======*/
+
+
+
 </style>
